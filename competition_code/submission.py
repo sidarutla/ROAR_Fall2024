57517ceb7d636f2e4956fa8e22f32664b7d35b7e
sidarutla <sidarutla@gmail.com>
2024-12-13 19:47:09 -0800

    Revert "324.25"
    This reverts commit b95ca14262006d7027c9860755473a33413c8d4d.

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -317,7 +317,7 @@ Steer: {control['steer']:.10f} \n"
             num_points = round(lookahead_value * 1.35)
         if self.current_section == 6:
             num_points = 5
-            next_waypoint_index = self.current_waypoint_idx + 27
+            next_waypoint_index = self.current_waypoint_idx + 25
         if self.current_section == 7:
             num_points = round(lookahead_value * 1.25)
         if self.current_section == 9:
